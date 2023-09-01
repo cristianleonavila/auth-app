9d78480dde236b8f61bc29866aaa948291237ae5
cristianleonavila <47152692+cristianleonavila@users.noreply.github.com>
2023-08-31 23:32:58 -0500

    Auth App Front End

---
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -0,0 +1,21 @@
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login-page',
+  templateUrl: './login-page.component.html',
+  styleUrls: ['./login-page.component.css']
+})
+export class LoginPageComponent {
+
+  private frmBuilder = inject( FormBuilder );
+   
+  public loginFrm: FormGroup = this.frmBuilder.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]]
+  });
+
+  login() {
+    console.log(this.loginFrm.value);
+  }
+}
