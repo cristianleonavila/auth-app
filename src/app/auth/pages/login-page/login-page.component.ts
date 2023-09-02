51525c70af41ac580fcb1e117b733d4edfc3dac4
cristianleonavila <47152692+cristianleonavila@users.noreply.github.com>
2023-09-01 23:58:30 -0500

    Login

--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login-page',
@@ -9,6 +10,8 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class LoginPageComponent {
 
   private frmBuilder = inject( FormBuilder );
+
+  private authServ: AuthService = inject( AuthService );
    
   public loginFrm: FormGroup = this.frmBuilder.group({
     email: ['', [Validators.required, Validators.email]],
@@ -16,6 +19,9 @@ export class LoginPageComponent {
   });
 
   login() {
-    console.log(this.loginFrm.value);
+    const { email, password } = this.loginFrm.value;
+    this.authServ.login( email, password ).subscribe( data => {
+      console.log(data);
+    });
   }
 }
